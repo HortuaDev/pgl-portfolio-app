7d39e30997869f3a5d32531e4ce06d36e69644c7
hortuadev <hortuadev@gmail.com>
2025-10-08 20:06:09 +0100

    feat(QrImage.jsx): creado el compoente QrImage para contener el codigo qr que se obtendra en caso de que displayMyQR sea falsa

---
+++ components/QrImage.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const QrImage = () => {
+  return (
+    <View>
+      <Text>QrImage</Text>
+    </View>
+  );
+};
+
+export default QrImage;
+
+const styles = StyleSheet.create({});
