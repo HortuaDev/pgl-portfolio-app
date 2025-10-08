a585ca0a623b8deb1b40fbefcdcdb855bbdf44a1
hortuadev <hortuadev@gmail.com>
2025-10-08 20:10:00 +0100

    fix(QrImage.jsx): organizada la estructura interna del componente QrImage y sus estilos

--- components/QrImage.tsx
+++ components/QrImage.tsx
@@ -1,14 +1,33 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import React from "react";
+import QRCode from "react-native-qrcode-svg";
 
 const QrImage = () => {
   return (
-    <View>
-      <Text>QrImage</Text>
+    <View style={styles.bodyStyles}>
+      <View style={styles.QrContainer}>
+        <QRCode value="https://github.com/adhernea" />
+      </View>
     </View>
   );
 };
 
 export default QrImage;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  bodyStyles: {
+    width: "100%",
+    borderWidth: 2,
+    borderColor: "black",
+    alignItems: "center",
+    justifyContent: "space-between",
+    height: "85%",
+  },
+  QrContainer: {
+    justifyContent: "center",
+    borderWidth: 1,
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+  },
+});
